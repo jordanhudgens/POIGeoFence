84dd4838db15430cbcf988b70c0384b6d27d6911
Jordan Hudgens <jordan@wow.com>
2014-08-30 00:19:02 -0500

    Initial Commit

---
+++ POIGeoFence/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  POIGeoFence
+//
+//  Created by Jordan Hudgens on 8/30/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
