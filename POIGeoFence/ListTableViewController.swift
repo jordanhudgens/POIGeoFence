e6de08b38b55186bd5b90b7c239904c947eb118b
Jordan Hudgens <jordan@wow.com>
2014-08-30 22:10:57 -0500

    debugging the google poi code

--- POIGeoFence/ListTableViewController.swift
+++ POIGeoFence/ListTableViewController.swift
@@ -26,7 +26,46 @@ class ListTableViewController: UIViewController, UISearchBarDelegate {
     
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
-        println(searchBar.text)
+        
+        searchGoogle("cafe")
+    }
+    
+    
+    
+    // API Stuff here
+    
+
+    
+    func searchGoogle(forContent: String) {
+        
+        let kGOOGLE_API_KEY: String! = "AIzaSyAms5WELg7IHAGeU-X2AvqqRTjBjYG-tp0"
+        //    let url = NSURL(string: ("https://maps.googleapis.com/maps/api/place/search/json?types=\(googleType)&location=\(currentCentre.latitude),\(currentCentre.longitude)&rankby=distance&sensor=true&key=\(kGOOGLE_API_KEY)"))
+        
+        // Cleaning the string
+        var spacelessString = forContent.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = NSURL(string: ("https://maps.googleapis.com/maps/api/place/search/json?types=\(spacelessString)&location=33.4884450,-101.9122650&rankby=distance&sensor=true&key=\(kGOOGLE_API_KEY)"))
+        
+        var session = NSURLSession.sharedSession()
+        
+        var task = session.dataTaskWithURL(urlPath) {
+            data, response, error -> Void in
+            
+            if ((error) != nil) {
+                println(error.localizedDescription)
+            }
+            
+            var jsonError: NSError?
+            
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as Dictionary<String, String>
+            
+            println(jsonResult)
+            
+            if ((jsonError?) != nil) {
+                println(jsonError!.localizedDescription)
+            }
+        }
+        
+        task.resume()
     }
 
 }
